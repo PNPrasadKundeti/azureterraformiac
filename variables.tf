700f59354bad7f04352db50869281063811531dd
sudhansagar <sudhansagar@gmail.com>
2020-07-28 14:48:13 +0530

    vm name variable added

--- variables.tf
+++ variables.tf
@@ -2,3 +2,11 @@ variable "az_subscription_id" {}
 variable "location"{
     default = "southeastasia"
 }
+
+variable "prefix" {
+  default = "example-machine"
+}
+
+locals {
+  vm_name = "${var.prefix}-vm"
+}
